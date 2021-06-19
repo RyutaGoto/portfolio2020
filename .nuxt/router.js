6bd87e412d9762fff1122870cb236fad2c810f91
RyutaGoto <b1016230@fun.ac.jp>
2021-06-19 18:57:06 +0900

    build

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,10 +3,10 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _4dc5ead8 = () => interopDefault(import('../pages/about.vue' /* webpackChunkName: "pages/about" */))
-const _dd32c868 = () => interopDefault(import('../pages/works/index.vue' /* webpackChunkName: "pages/works/index" */))
-const _77ad8e2c = () => interopDefault(import('../pages/works/_detail.vue' /* webpackChunkName: "pages/works/_detail" */))
-const _0e56354e = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+const _b72747d0 = () => interopDefault(import('../pages/about.vue' /* webpackChunkName: "pages/about" */))
+const _f6288b60 = () => interopDefault(import('../pages/works/index.vue' /* webpackChunkName: "pages/works/index" */))
+const _2a3e7324 = () => interopDefault(import('../pages/works/_detail.vue' /* webpackChunkName: "pages/works/_detail" */))
+const _77b79246 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -19,26 +19,26 @@ Vue.use(Router)
 
 export const routerOptions = {
   mode: 'history',
-  base: decodeURI('/portfolio2020/'),
+  base: decodeURI('/portfolio/'),
   linkActiveClass: 'nuxt-link-active',
   linkExactActiveClass: 'nuxt-link-exact-active',
   scrollBehavior,
 
   routes: [{
     path: "/about",
-    component: _4dc5ead8,
+    component: _b72747d0,
     name: "about"
   }, {
     path: "/works",
-    component: _dd32c868,
+    component: _f6288b60,
     name: "works"
   }, {
     path: "/works/:detail",
-    component: _77ad8e2c,
+    component: _2a3e7324,
     name: "works-detail"
   }, {
     path: "/",
-    component: _0e56354e,
+    component: _77b79246,
     name: "index"
   }],
 
--- .nuxt/utils.js
+++ .nuxt/utils.js
@@ -149,7 +149,7 @@ export async function setContext (app, context) {
       store: app.store,
       payload: context.payload,
       error: context.error,
-      base: '/portfolio2020/',
+      base: '/portfolio/',
       env: {}
     }
     // Only set once
